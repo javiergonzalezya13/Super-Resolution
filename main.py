0f96db5d5337693e44a24249abd7c5b66703fdde
javiergonzalezya13 <39286264+javiergonzalezya13@users.noreply.github.com>
2021-04-18 20:24:01 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ import datetime
 import os
 import sys
 
+import keras.backend as K
 import yaml
 
-from FRVSR import *
-from TecoGAN import *
-from EDVR import EDVR
-from EDVR_v2 import EDVR as EDVR_v2
+# from EDVR import EDVR
+# from EDVR_v2 import EDVR as EDVR_v2
+from FRVSR import FrameRecurrentVideoSR
+from TecoGAN import TecoGAN
 from yoloV3_predict import YoloV3
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def parse_args():
-    ap = argparse.ArgumentParser(description='Frame-Recurrent Video Super Resolution')
+    ap = argparse.ArgumentParser(description='Video Super-Resolution')
 
     ap.add_argument('--yaml_file', help='YAML file.')
 
@@ -25,68 +26,61 @@ def parse_args():
     return args
 
 if __name__ == '__main__':
+    # Get yaml file parameters
     args = parse_args()
-
     with open(args.yaml_file) as file:
         configs = yaml.safe_load(file)
 
-
-    LR_shape = (configs['data']['low_res'],
+    # Shape parameters and upscale
+    LR_SHAPE = (configs['data']['low_res'],
                 configs['data']['low_res'],
                 configs['data']['channels'])
-    HR_shape = (configs['data']['high_res'],
+    HR_SHAPE = (configs['data']['high_res'],
                 configs['data']['high_res'],
                 configs['data']['channels'])
-    upscale = configs['data']['upscale']
+    UPSCALE = configs['data']['upscale']
 
+    if (LR_SHAPE[0] * UPSCALE != HR_SHAPE[0]) or (LR_SHAPE[1] * UPSCALE != HR_SHAPE[1]):
+        print('[ERROR] Incompatible resolutions with upscale x%d.' % (UPSCALE))
+        sys.exit(0)
+
+    # Create directory
     NOW = datetime.datetime.now()
     OUTPUT_DIR = '%d-%d-%d %d:%d:%d' % (NOW.year, NOW.month, NOW.day,
                                         NOW.hour, NOW.minute, NOW.second)
 
-    if not configs['data']['videos']:
-        print('[ERROR] No data available.')
-        sys.exit(0)
-
     OUTPUT_DIR = os.path.join(configs['root_dir'], 'MODEL', OUTPUT_DIR)
 
-    if (LR_shape[0] * upscale != HR_shape[0]) or (LR_shape[1] * upscale != HR_shape[1]):
-        print('[ERROR] Incompatible resolutions with upscale x%d.' % (upscale))
-        sys.exit(0)
 
-    # MODEL
-    # if configs['stage']['train'] or configs['stage']['eval'] or configs['stage']['run'] or configs['stage']['run_image']:
+    # Check neuronal network to use
     if configs['stage']['train'] or configs['stage']['eval'] or configs['stage']['run']:
         if configs['model'] == 'frvsr':
-            model = FrameRecurrentVideoSR(LR_shape, HR_shape, OUTPUT_DIR, configs)
+            MODEL = FrameRecurrentVideoSR(LR_SHAPE, HR_SHAPE, OUTPUT_DIR, configs)
         elif configs['model'] == 'tecogan':
-            model = TecoGAN(LR_shape, HR_shape, OUTPUT_DIR, configs)
-        elif configs['model'] == 'edvr':
-            model = EDVR(LR_shape, HR_shape, OUTPUT_DIR, configs)
-        elif configs['model'] == 'edvr_v2':
-            model = EDVR_v2(configs, OUTPUT_DIR, inp_shape=LR_shape, nframes=5)
+            MODEL = TecoGAN(LR_SHAPE, HR_SHAPE, OUTPUT_DIR, configs)
+        # elif configs['model'] == 'edvr':
+        #     MODEL = EDVR(LR_SHAPE, HR_SHAPE, OUTPUT_DIR, configs)
+        # elif configs['model'] == 'edvr_v2':
+        #     MODEL = EDVR_v2(configs, OUTPUT_DIR, inp_shape=LR_SHAPE, nframes=5)
         else:
             print('[ERROR] Not valid model selected.')
             sys.exit(0)
-        model.build()
+        MODEL.build()
 
-    # TRAINING
+    # Train model
     if configs['stage']['train']:
-        model.train()
+        MODEL.train()
 
-    # EVALUATION
+    # Evaluate model
     if configs['stage']['eval']:
-        model.eval()
+        MODEL.eval()
 
-    # RUN
+    # Run single video or camera
     if configs['stage']['run']:
-        # model.run(configs)
-        model.run()
+        MODEL.run()
 
-    # if configs['stage']['run_image']:
-    #     model.run_image()
-
-    # RECOGNITION
+    # Recognition with YOLO V3
     K.clear_session()
     if configs['stage']['rec']:
-        model = YoloV3(configs)
-        model.predict()
+        YOLO_MODEL = YoloV3(configs)
+        YOLO_MODEL.predict()
